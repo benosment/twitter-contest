eb0224ed59606c5042723c7e6f47337cd547ce08
Benjamin Osment <benjamin.j.osment@gmail.com>
2015-08-18 18:31:15 -0400

    add (failing) UT testcases for database, start to refactor to a class

---
+++ database.py
@@ -0,0 +1,21 @@
+class Database():
+    def __init__(self, db_name):
+        pass
+
+    def add_tweet(self, tweet_id, tweet_content):
+        pass
+
+    def get_tweet(self, tweet_id):
+        pass
+
+    def all_tweets(self):
+        pass
+
+    def add_user(self, user_id, user_name):
+        pass
+
+    def get_user(self, user_id):
+        pass
+
+    def all_users(self):
+        pass
--- tests.py
+++ tests.py
@@ -5,8 +5,10 @@ import unittest
 import tcontest
 import settings
 
+from database import Database
 
 class TwitterUnitTest(unittest.TestCase):
+
     def setUp(self):
         self.api = tcontest.oauth_login(settings.TWITTER_CONSUMER_KEY,
                                         settings.TWITTER_CONSUMER_SECRET,
@@ -15,6 +17,32 @@ class TwitterUnitTest(unittest.TestCase):
 
     def test_authenticated(self):
         self.assertEqual(self.api.me().name, 'Benjamin Osment')
+
+
+class DBUnitTest(unittest.TestCase):
+
+    def setUp(self):
+        self.db = Database('test.db')
+
+    def test_add_tweets(self):
+        self.db.add_tweet('1', 'hello world')
+        self.db.add_tweet('2', 'hello world')
+        self.assertEqual(len(self.db.all_tweets()), 2)
+
+    def test_add_users(self):
+        self.db.add_user('1', 'jinn')
+        self.db.add_user('2', 'steve')
+        self.assertEqual(len(self.db.all_users()), 2)
+
+    def test_query_tweet(self):
+        self.db.add_tweet('1', 'hello world')
+        self.assertEqual(self.db.get_tweet('1'), 1)
+        self.assertEqual(self.db.get_tweet('3'), None)
+
+    def test_query_users(self):
+        self.db.add_user('1', 'jinn')
+        self.assertEqual(self.db.get_user('1'), 1)
+        self.assertEqual(self.db.get_user('3'), None)
 
 
 if __name__ == '__main__':

6b05ad781cb1573d2649f21e937b7b3cf469afb7
Benjamin Osment <benjamin.j.osment@gmail.com>
2015-08-19 11:06:33 -0400

    fully refactor Twitter to new class

--- tcontest.py
+++ tcontest.py
@@ -2,10 +2,11 @@
 
 import os
 import sys
-
+import settings
 import logging
 import logging.config
-
+from database import Database
+from twitter import Twitter
 
 def init_logger():
     logging.config.dictConfig(settings.LOGGING)
@@ -15,26 +16,4 @@ if __name__ == '__main__':
     init_logger()
     db = Database('tweet.db')
     t = Twitter(db)
-
-    # # TODO -- returns nothing?
-    # logging.debug('API rate limiting status', api.rate_limit_status())
-
-    # # TODO -- use max_id = (last_id)?
-    # found_tweets = api.search(q='RT win', count=100, lang='en')
-    # for count, tweet in enumerate(found_tweets):
-    #     if hasattr(tweet, 'retweeted_status'):
-    #         print("Retweet :: %d %s" % (count, tweet.text))
-    #         print("Original tweet was from %s and was %s" %
-    #               (tweet.retweeted_status.user.screen_name, tweet.retweeted_status.text))
-    #         if ('RT' in tweet.retweeted_status.text):
-    #             print("!!! Candidate for contest")
-    #             # TODO -- need to check if we have been friends before
-    #             # TODO -- need to check if we have tweeted this ID before
-    #             import pdb; pdb.set_trace()
-    #             api.create_friendship(tweet.retweeted_status.user.screen_name)
-    #             logging.debug('started following %s' % tweet.retweeted_status.user.screen_name)
-    #             api.retweet(tweet.retweeted_status.id)
-    #             logging.debug('retweeted %d : %s' % (tweet.retweeted_status.id,
-    #                                                  tweet.retweeted_status.text))
-    #     else:
-    #         print("Original :: %d %s" % (count, tweet.text))
+    t.search('RT win')
--- twitter.py
+++ twitter.py
@@ -29,3 +29,37 @@ class Twitter():
         auth.set_access_token(access_token, access_token_secret)
 
         return tweepy.API(auth)
+
+    def rate_limit_stats(self):
+        logging.debug('API rate limiting status', self.api.rate_limit_status())
+
+    def search(self, query):
+        found_tweets = self.api.search(q=query, count=100, lang='en')
+        for count, tweet in enumerate(found_tweets):
+            if hasattr(tweet, 'retweeted_status'):
+                logging.debug("Retweet :: %d %s" % (count, tweet.text))
+                logging.debug("Original tweet was from %s and was %s" %
+                              (tweet.retweeted_status.user.screen_name,
+                               tweet.retweeted_status.text))
+                if ('RT' in tweet.retweeted_status.text):
+                    logging.debug("!!! Candidate for contest")
+                    import pdb; pdb.set_trace()
+                    if ('follow' in tweet.retweeted_status.text.lower()):
+                        self.follow(tweet.retweeted_status.user.screen_name)
+                    self.retweet(tweet)
+            else:
+                print("Original :: %d %s" % (count, tweet.text))
+
+    def follow(self, user):
+        # TODO -- need to check if we have been friends before
+        self.api.create_friendship(user)
+        logging.debug('started following %s' % user)
+
+    def unfollow(self, user):
+        pass
+
+    def retweet(self, tweet):
+        # TODO -- need to check if we have tweeted this ID before
+        self.api.retweet(tweet.retweeted_status.id)
+        logging.debug('retweeted %d : %s' % (tweet.retweeted_status.id,
+                                             tweet.retweeted_status.text))
